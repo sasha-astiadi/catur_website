7caee1cb566ddbe4d9f9f3251069e5cc7573b407
sasha-astiadi <sasha@incubaid.com>
2026-03-09 18:45:18 +0800

    update back button to always show and navigate to /projects when no previous path

--- src/components/ProjectLayout.tsx
+++ src/components/ProjectLayout.tsx
@@ -39,16 +39,20 @@ export function ProjectLayout({
     <Container className="mt-16 lg:mt-24">
       <div className="xl:relative">
         <div className="mx-auto max-w-2xl">
-          {previousPathname && (
-            <button
-              type="button"
-              onClick={() => router.back()}
-              aria-label="Go back to articles"
-              className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md ring-1 shadow-zinc-800/5 ring-zinc-900/5 transition lg:absolute lg:-left-5 lg:-mt-2 lg:mb-0 xl:-top-1.5 xl:left-0 xl:mt-0 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20"
-            >
-              <ArrowLeftIcon className="h-4 w-4 stroke-zinc-500 transition group-hover:stroke-zinc-700 dark:stroke-zinc-500 dark:group-hover:stroke-zinc-400" />
-            </button>
-          )}
+          <button
+            type="button"
+            onClick={() => {
+              if (previousPathname) {
+                router.back()
+              } else {
+                router.push('/projects')
+              }
+            }}
+            aria-label="Go back to articles"
+            className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md ring-1 shadow-zinc-800/5 ring-zinc-900/5 transition lg:absolute lg:-left-5 lg:-mt-2 lg:mb-0 xl:-top-1.5 xl:left-0 xl:mt-0 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20"
+          >
+            <ArrowLeftIcon className="h-4 w-4 stroke-zinc-500 transition group-hover:stroke-zinc-700 dark:stroke-zinc-500 dark:group-hover:stroke-zinc-400" />
+          </button>
           <article>
             <header className="flex flex-col">
               <h1 className="mt-6 text-4xlfont-normal tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
